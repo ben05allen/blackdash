c7d75c1f1f98652d3064feb69e651524e9a53a57
ben05allen <ben05allen@hotmail.com>
2024-10-04 22:05:56 +0900

    async tickers

--- data/generate_csv.py
+++ data/generate_csv.py
@@ -1,37 +1,92 @@
+import asyncio
 import csv
+from dataclasses import dataclass
+import httpx
+from itertools import islice
+from lxml import etree
 import tomllib
-
-from scraper import scrape_tickers
+from typing import AsyncGenerator
 
 CSV_FILE = "data/test.csv"
-CSV_FIELDS = [
-    "symbol",
-    "name",
-    "stockindex",
-    "exchange",
-    "currency",
-]
 
 
-with open("data/exchanges.toml", "rb") as exchanges_file, open(
-    CSV_FILE, "w"
-) as csv_file:
-    exchanges = tomllib.load(exchanges_file)
-    csv_writer = csv.DictWriter(csv_file, fieldnames=CSV_FIELDS)
-    csv_writer.writeheader()
+@dataclass
+class ExchangeMeta:
+    stockindex: str
+    exchange: str
+    currency: str
+    url: str
+    tickers_xpath: str
+    names_xpath: str
+    ticker_suffix: str
+    limit: int
 
-    for exch_details in exchanges.values():
-        company = dict.fromkeys(CSV_FIELDS)
-        company["stockindex"] = exch_details["StockIndex"]
-        company["exchange"] = exch_details["Exchange"]
-        company["currency"] = exch_details["Currency"]
 
-        for name, ticker in scrape_tickers(
-            exch_details["URL"],
-            exch_details["TickersXPath"],
-            exch_details["NamesXPath"],
-            exch_details["Limit"],
-        ):
-            company["symbol"] = f"{ticker}{exch_details.get('TickerSuffix','')}"
-            company["name"] = name
-            csv_writer.writerow(company)
+@dataclass
+class Ticker:
+    name: str
+    symbol: str
+    stockindex: str
+    exchange: str
+    currency: str
+
+
+async def scrape_tickers(
+    client, url: str, tickers_xpath: str, names_xpath: str, limit: int
+) -> AsyncGenerator[tuple[str, str], None]:
+    parser = etree.HTMLParser()
+    response = await client.get(url)
+    root = etree.fromstring(response.content, parser)
+    tickers = root.xpath(tickers_xpath)
+    names = root.xpath(names_xpath)
+
+    return [(n, t) for n, t in islice(zip(names, tickers), limit)]
+
+
+async def create_exch_list(client, meta: ExchangeMeta) -> list[Ticker]:
+    exchange_list = []
+    tickers_list = await scrape_tickers(
+        client, meta.url, meta.tickers_xpath, meta.names_xpath, meta.limit
+    )
+    for name, ticker in tickers_list:
+        exchange_list.append(
+            Ticker(
+                name,
+                f"{ticker}{meta.ticker_suffix}",
+                meta.stockindex,
+                meta.exchange,
+                meta.currency,
+            )
+        )
+    return exchange_list
+
+
+async def gather_data() -> list[list[Ticker]]:
+    with open("data/exchanges.toml", "rb") as f:
+        exch_data = tomllib.load(f)
+
+    async with httpx.AsyncClient() as client:
+        tasks = [
+            create_exch_list(client, ExchangeMeta(**exch))
+            for exch in exch_data.values()
+        ]
+        return await asyncio.gather(*tasks)
+
+
+def write_csv(exchanges_data: list[list]) -> None:
+    with open(CSV_FILE, "w") as f:
+        csv_writer = csv.DictWriter(f, fieldnames=Ticker.__dataclass_fields__)
+        csv_writer.writeheader()
+
+        for exchange_list in exchanges_data:
+            for ticker in exchange_list:
+                csv_writer.writerow(ticker.__dict__)
+
+
+async def main() -> None:
+    exchanges_data = await gather_data()
+    write_csv(exchanges_data)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- data/scraper.py
+++ data/scraper.py
@@ -1,30 +1,16 @@
+import httpx
 from itertools import islice
 from lxml import etree
-import requests
-from typing import Generator
 
 
-def scrape_tickers(
+async def scrape_tickers(
     url: str, tickers_xpath: str, names_xpath: str, limit: int
-) -> Generator[tuple[str, str], None, None]:
+) -> list[tuple[str, str]]:
     parser = etree.HTMLParser()
-    response = requests.get(url)
-    root = etree.fromstring(response.content, parser)
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        root = etree.fromstring(response.content, parser)
+        tickers = root.xpath(tickers_xpath)
+        names = root.xpath(names_xpath)
 
-    tickers = root.xpath(tickers_xpath)
-    names = root.xpath(names_xpath)
-
-    for name, ticker in islice(zip(names, tickers), limit):
-        yield (name.strip(), ticker.strip())
-
-
-if __name__ == "__main__":
-    kospi_url = "https://stockanalysis.com/list/korea-stock-exchange/"
-    kospi_tickers_xpath = '//td[@class="sym svelte-eurwtr"]/a/text()'
-    kospi_names_xpath = '//td[@class="slw svelte-eurwtr"]/text()'
-    kospi_limit = 20
-
-    for name, ticker in scrape_tickers(
-        kospi_url, kospi_tickers_xpath, kospi_names_xpath, kospi_limit
-    ):
-        print(f"{name} ({ticker}.KS)")
+        return [(n, t) for n, t in islice(zip(names, tickers), limit)]
