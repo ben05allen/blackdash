a07bbb19afa00785bbd5b6c25ecc9a34bc69ff64
ben05allen <ben05allen@hotmail.com>
2024-10-04 14:22:01 +0900

    better source for kospi tickers

--- data/scrape_kopsi.py
+++ data/scrape_kopsi.py
@@ -4,25 +4,22 @@ import requests
 from typing import Generator
 
 
-# Unfortunately we're not getting purely KOSPI tickers....
-# For example Hyundai's ticker comes back as HYMTF rather than 005380.KS
-
-URL = "https://companiesmarketcap.com/south-korea/largest-companies-in-south-korea-by-market-cap/"
-COMPANIES = 5
+URL = "https://stockanalysis.com/list/korea-stock-exchange/"
+LIMIT = 5
 
 
 def scrape_tickers(url: str, limit: int) -> Generator[tuple[str, str], None, None]:
-    response = requests.get(url)
+    response = requests.get(URL)
     parser = etree.HTMLParser()
     root = etree.fromstring(response.content, parser)
 
-    company_name = root.xpath('//div[@class="company-name"]/text()')
-    company_code = root.xpath('//div[@class="company-code"]/text()')
+    tickers = root.xpath('//td[@class="sym svelte-eurwtr"]/a/text()')
+    names = root.xpath('//td[@class="slw svelte-eurwtr"]/text()')
 
-    for name, code in islice(zip(company_name, company_code), limit):
-        yield (name.strip(), code.strip())
+    for name, ticker in islice(zip(names, tickers), limit):
+        yield (name.strip(), ticker.strip())
 
 
 if __name__ == "__main__":
-    for name, code in scrape_tickers(URL, COMPANIES):
-        print(f"{name} ({code})")
+    for name, ticker in scrape_tickers(URL, LIMIT):
+        print(f"{name} ({ticker}.KS)")
